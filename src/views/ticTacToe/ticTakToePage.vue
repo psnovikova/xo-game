8d0d44e47df01c8e4475ffd22311b01ac78358bc
psnovikova <nps20052000@mail.ru>
2023-11-18 14:54:15 +0300

    chore: realise play logic

--- src/views/ticTacToe/ticTakToePage.vue
+++ src/views/ticTacToe/ticTakToePage.vue
@@ -1,48 +1,109 @@
 <script setup lang="ts">
-import {ref} from "vue";
+import {computed, ref} from "vue";
+import ConfettiExplosion from "vue-confetti-explosion";
+
+type FigerType = 'circle' | 'cross'
 
 interface cellParam {
   id: number,
   isClicked: boolean
+  figure: FigerType | null
 }
 
 const cellData = ref<cellParam[]>([
-  {id: 1, isClicked: false},
-  {id: 2, isClicked: false},
-  {id: 3, isClicked: false},
-  {id: 4, isClicked: false},
-  {id: 5, isClicked: false},
-  {id: 6, isClicked: false},
-  {id: 7, isClicked: false},
-  {id: 8, isClicked: false},
-  {id: 9, isClicked: false},
+  {id: 1, isClicked: false, figure: null},
+  {id: 2, isClicked: false, figure: null},
+  {id: 3, isClicked: false, figure: null},
+  {id: 4, isClicked: false, figure: null},
+  {id: 5, isClicked: false, figure: null},
+  {id: 6, isClicked: false, figure: null},
+  {id: 7, isClicked: false, figure: null},
+  {id: 8, isClicked: false, figure: null},
+  {id: 9, isClicked: false, figure: null},
 ])
+
+const lastSelectedFigure = ref<FigerType>('cross')
+
+const isWinCombination = computed(() => {
+  const winCombinations = [
+    [1, 2, 3],
+    [1, 4, 7],
+    [1, 5, 9],
+    [2, 5, 8],
+    [3, 6, 9],
+    [3, 5, 7],
+    [4, 5, 6],
+    [7, 8, 9],
+  ]
+
+  if (cellData.value.filter(cell => cell.isClicked).length < 5) return false
+  else {
+    const circleCells = cellData.value.filter(cell => cell.isClicked && cell.figure === 'circle').map(cell => cell.id)
+    const crossCells = cellData.value.filter(cell => cell.isClicked && cell.figure === 'cross').map(cell => cell.id)
+
+    const isCircleWin = winCombinations.some(combination => {
+      return combination.every(cell => circleCells.includes(cell) && combination)
+    })
+
+    const isCrossWin = winCombinations.some(combination => {
+      return combination.every(cell => crossCells.includes(cell))
+    })
+
+    if (isCircleWin) {
+      return winCombinations.find(combination => {
+        return combination.every(cell => circleCells.includes(cell))
+      })
+    }
+
+    if (isCrossWin) {
+      return winCombinations.find(combination => {
+        return combination.every(cell => crossCells.includes(cell))
+      })
+    }
+  }
+})
+
 
 function cellClicked(param: cellParam) {
   param.isClicked = true
+  lastSelectedFigure.value = lastSelectedFigure.value === 'cross' ? 'circle' : 'cross'
+  param.figure = lastSelectedFigure.value
 }
 </script>
 
 <template>
   <div class="w-screen h-screen bg-[#FAD074] overflow-hidden">
-    <div class="w-full h-full flex justify-center items-center">
-      <div class="grid-container grid grid-rows-3 grid-cols-3 justify-center items-center gap-0 w-[30rem] h-[30rem]">
+    <div class="relative w-full h-full flex justify-center items-center">
+      <span class="absolute top-1/2 right-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <ConfettiExplosion v-if="isWinCombination"/>
+      </span>
+      <div
+          class="grid-container grid grid-rows-3 grid-cols-3 justify-center items-center gap-0 w-[30rem] h-[30rem]"
+      >
         <button
             v-for="param in cellData"
             :key="param.id"
+            :disabled="param.isClicked || !!isWinCombination"
             class="w-full h-full flex items-center justify-center"
-            :class="{ 'hover:bg-[#FFA570]': !param.isClicked}"
+            :class="{ 'hover:bg-[#8DC089]': !param.isClicked && !isWinCombination}"
             @click="cellClicked(param)"
         >
-          <canvas
-              v-if="param.isClicked"
-              class="circle w-[8rem] h-[8rem] border-2 border-[#FF6933] rounded-full"
+          <span
+              v-if="param.isClicked && param.figure === 'circle'"
+              class="circle w-[8rem] h-[8rem] border-4 border-[#FF6933] rounded-full"
+              :class="{'spin': isWinCombination && isWinCombination.includes(param.id)}"
           />
+          <span v-if="param.isClicked && param.figure === 'cross'"
+                class="w-[8rem] h-[8rem] relative"
+                :class="{'spin': isWinCombination && isWinCombination.includes(param.id)}"
+          >
+            <span class="absolute w-full h-1 rotate-45 bg-[#2D4628] top-1/2 left-0"/>
+            <span class="absolute w-full h-1 -rotate-45 bg-[#2D4628] top-1/2 left-0"/>
+          </span>
         </button>
       </div>
     </div>
   </div>
-  e
 </template>
 
 <style scoped>
@@ -70,6 +131,27 @@ function cellClicked(param: cellParam) {
 
 .circle {
   animation: draw 2s ease-in-out;
+}
+
+.circle {
+  transition: transform 0.5s ease-in-out;
+}
+
+.circle:hover {
+  transform: rotate(360deg);
+}
+
+.spin {
+  animation: spin 4s linear infinite;
+}
+
+@keyframes spin {
+  from {
+    transform: rotateY(0deg);
+  }
+  to {
+    transform: rotateY(360deg);
+  }
 }
 
 @keyframes show {
